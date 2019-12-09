925b122b45500a06784e232804849e2b0bc92ab3
Dan Thomas <dthomas@ddti.net>
2019-12-09 15:01:50 -0500

    Adding code.

---
+++ Nu.Messaging/IMessageRouter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Nu.Messaging
+{
+    public interface IMessageRouter
+    {
+        void Publish<T>(T message, string key);
+
+        TR RemoteCall<T, TR>(T message);
+
+        void Subscribe<T>(Action<T> callback, string key);
+
+        void RegisterRemoteCall<T, TR>(Func<T, TR> callback);
+
+        void SubscribeClass(object obj);
+    }
+}
---
+++ Nu.Messaging/SubscriptionAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Nu.Messaging
+{
+    public class SubscriptionAttribute : Attribute
+    {
+        public SubscriptionAttribute(string routingKey)
+        {
+            RoutingKey = routingKey;
+        }
+
+        public string RoutingKey { get; }
+    }
+}
